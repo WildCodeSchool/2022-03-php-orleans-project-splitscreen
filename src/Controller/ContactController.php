28b754fabb8065e90195a66d435aa15796ebb5d6
codelande <corentin-delande_student2022@wilder.school>
2022-06-08 11:27:43 +0200

    mail sending

--- src/Controller/ContactController.php
+++ src/Controller/ContactController.php
@@ -13,13 +13,20 @@ use Symfony\Component\Routing\Annotation\Route;
 class ContactController extends AbstractController
 {
     #[Route('/contact', name: 'app_contact')]
-    public function index(Request $request,): Response
+    public function index(Request $request, MailerInterface $mailer): Response
     {
         $form = $this->createForm(ContactType::class);
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
-            $this->addFlash('success', 'Votre message a bien etait envoyé');
+            $contactFormData = $form->getData();
+            $email = (new Email())
+                ->from($contactFormData['email'])
+                ->to('test@test.com')
+                ->subject('Message split screen')
+                ->html($this->renderView('contact/_email.html.twig', ['form' => $contactFormData]));
+            $mailer->send($email);
+            $this->addFlash('success', 'Votre message a bien été envoyé');
             return $this->redirectToRoute('app_contact');
         }
 
