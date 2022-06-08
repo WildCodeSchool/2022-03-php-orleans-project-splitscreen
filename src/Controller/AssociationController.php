7be063a5c74041b4d2321d5bfa7c70a7d4917a72
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-06-08 10:51:56 +0200

    Creation of controller and template

---
+++ src/Controller/AssociationController.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+class AssociationController extends AbstractController
+{
+    #[Route('/association', name: 'app_association')]
+    public function index(): Response
+    {
+        return $this->render('association/index.html.twig', [
+            'controller_name' => 'AssociationController',
+        ]);
+    }
+}
