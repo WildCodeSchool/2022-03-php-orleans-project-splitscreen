7f6f10ade149ae55fc8b293c2e6bc59f7dda570f
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-06-09 16:10:59 +0200

    new scss for asso and a few fixes

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,9 @@ Encore
      * and one CSS file (e.g. app.css) if your JavaScript imports CSS.
      */
     .addEntry('app', './assets/app.js')
+    .addStyleEntry('association', './assets/styles/association.scss')
+
+    
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
