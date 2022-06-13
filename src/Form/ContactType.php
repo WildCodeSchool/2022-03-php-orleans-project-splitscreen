1c11017f09e12a8ba7f2d380f1f2c00e824848f0
codelande <corentin-delande_student2022@wilder.school>
2022-06-13 10:24:28 +0200

    commented code

--- src/Form/ContactType.php
+++ src/Form/ContactType.php
@@ -39,15 +39,11 @@ class ContactType extends AbstractType
 
     public function configureOptions(OptionsResolver $resolver): void
     {
-        // $resolver->setDefaults([
-        //     'data_class'      => ContactType::class,
-        //     // enable/disable CSRF protection for this form
-        //     'csrf_protection' => true,
-        //     // the name of the hidden HTML field that stores the token
-        //     'csrf_field_name' => '_token',
-        //     // an arbitrary string used to generate the value of the token
-        //     // using a different string for each form improves its security
-        //     'csrf_token_id'   => 'contact_item',
-        // ]);
+        $resolver->setDefaults([
+            'data_class'      => ContactType::class,
+            'csrf_protection' => true,
+            'csrf_field_name' => '_token',
+            'csrf_token_id'   => 'contact_item',
+        ]);
     }
 }
