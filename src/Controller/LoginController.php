16c912d29f86c55d98b3b232abda83ac3ba5bc72
codelande <corentin-delande_student2022@wilder.school>
2022-06-13 17:20:24 +0200

    CT

--- src/Controller/LoginController.php
+++ src/Controller/LoginController.php
@@ -9,7 +9,7 @@ use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class LoginController extends AbstractController
 {
-    #[Route('/connexion', name: 'connexion')]
+    #[Route('/connexion', name: 'connection')]
     public function index(AuthenticationUtils $authenticationUtils): Response
     {
         // get the login error if there is one
