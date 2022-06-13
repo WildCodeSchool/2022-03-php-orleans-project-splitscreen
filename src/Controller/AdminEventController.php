e6e6d93964e64bbd1790188cbf6bd5e1def19848
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-06-13 17:26:15 +0200

    event to evenements

--- src/Controller/AdminEventController.php
+++ src/Controller/AdminEventController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/event')]
+#[Route('/admin/evenements')]
 class AdminEventController extends AbstractController
 {
     #[Route('/', name: 'app_admin_event_index', methods: ['GET'])]
