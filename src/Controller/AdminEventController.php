e06e20069c90f924c4d8aa0b7f4200ab38acee15
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-06-13 16:28:29 +0200

    added CRUD

---
+++ src/Controller/AdminEventController.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Event;
+use App\Form\EventType;
+use App\Repository\EventRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/admin/event')]
+class AdminEventController extends AbstractController
+{
+    #[Route('/', name: 'app_admin_event_index', methods: ['GET'])]
+    public function index(EventRepository $eventRepository): Response
+    {
+        return $this->render('admin_event/index.html.twig', [
+            'events' => $eventRepository->findAll(),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_admin_event_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, EventRepository $eventRepository): Response
+    {
+        $event = new Event();
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $eventRepository->add($event, true);
+
+            return $this->redirectToRoute('app_admin_event_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_event/new.html.twig', [
+            'event' => $event,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_event_show', methods: ['GET'])]
+    public function show(Event $event): Response
+    {
+        return $this->render('admin_event/show.html.twig', [
+            'event' => $event,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_admin_event_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Event $event, EventRepository $eventRepository): Response
+    {
+        $form = $this->createForm(EventType::class, $event);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $eventRepository->add($event, true);
+
+            return $this->redirectToRoute('app_admin_event_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('admin_event/edit.html.twig', [
+            'event' => $event,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_admin_event_delete', methods: ['POST'])]
+    public function delete(Request $request, Event $event, EventRepository $eventRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $event->getId(), $request->request->get('_token'))) {
+            $eventRepository->remove($event, true);
+        }
+
+        return $this->redirectToRoute('app_admin_event_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/EventType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Event;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class EventType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('title')
+            ->add('date')
+            ->add('image')
+            ->add('description')
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Event::class,
+        ]);
+    }
+}
