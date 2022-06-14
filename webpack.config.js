2e35577c3967b844d392984d3d25fca58de1bd86
Mathias Bretonneau <Mathias.bretonneau@gmail.com>
2022-06-14 17:04:23 +0200

    Fix Navbar Logo and style

--- webpack.config.js
+++ webpack.config.js
@@ -34,6 +34,7 @@ Encore
     .addEntry('app', './assets/app.js')
     .addStyleEntry('event', './assets/styles/event.scss')
     .addStyleEntry('header', './assets/styles/header.scss')
+    .addStyleEntry('navbar', './assets/styles/navbar.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
