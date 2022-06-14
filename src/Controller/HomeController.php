9c29326a249d17eb7e3db16c428d42cca1ae1b0e
codelande <corentin-delande_student2022@wilder.school>
2022-06-14 16:54:42 +0200

    all done :)

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -12,7 +12,11 @@ class HomeController extends AbstractController
     #[Route('/', name: 'home_index')]
     public function index(EventRepository $eventRepository): Response
     {
-        $events = $eventRepository->findAll();
+        $events = $eventRepository->findBy(
+            [],
+            ['date' => 'DESC'],
+            3
+        );
         return $this->render('home/index.html.twig', ['events' => $events,]);
     }
 }
