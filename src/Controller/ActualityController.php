a132c388b1282bf3ecb74c391657d63d83453b26
Mathias Bretonneau <Mathias.bretonneau@gmail.com>
2022-06-16 12:23:28 +0200

    FIX

--- src/Controller/ActualityController.php
+++ src/Controller/ActualityController.php
@@ -7,7 +7,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/actualité', name: 'actuality_')]
+#[Route('/actualite', name: 'actuality_')]
 class ActualityController extends AbstractController
 {
     #[Route('/', name: 'index')]
