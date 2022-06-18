a8b92589c9e8279f9e184529dd0d2b038fd1144b
daniel <daniel@daniel-Inspiron-7786>
2022-06-18 10:40:50 +0200

    ajout des actualités dans la page home

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ActualityRepository;
 use App\Repository\EventRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,13 +11,21 @@ use Symfony\Component\Routing\Annotation\Route;
 class HomeController extends AbstractController
 {
     #[Route('/', name: 'home_index')]
-    public function index(EventRepository $eventRepository): Response
+    public function index(EventRepository $eventRepository, ActualityRepository $actualityRepository): Response
     {
         $events = $eventRepository->findBy(
             [],
             ['date' => 'DESC'],
             3
         );
-        return $this->render('home/index.html.twig', ['events' => $events,]);
+        $actus = $actualityRepository->findBy(
+            [],
+            ['date' => 'DESC'],
+            3
+        );
+        return $this->render('home/index.html.twig', [
+            'events' => $events,
+            'actus' => $actus,
+        ]);
     }
 }
