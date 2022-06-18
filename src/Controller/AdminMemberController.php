5a3ce10687d235a788011cf6c9cd988cc5da4d6e
daniel <daniel@daniel-Inspiron-7786>
2022-06-18 12:51:05 +0200

    début de la vue

--- src/Controller/AdminMemberController.php
+++ src/Controller/AdminMemberController.php
@@ -10,18 +10,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/member')]
+#[Route('/admin/membres', name: 'admin_member_')]
 class MemberController extends AbstractController
 {
-    #[Route('/', name: 'app_member_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(MemberRepository $memberRepository): Response
     {
-        return $this->render('member/index.html.twig', [
+        return $this->render('admin/admin_member/index.html.twig', [
             'members' => $memberRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_member_new', methods: ['GET', 'POST'])]
+    #[Route('/ajouter', name: 'add', methods: ['GET', 'POST'])]
     public function new(Request $request, MemberRepository $memberRepository): Response
     {
         $member = new Member();
@@ -31,24 +31,24 @@ class MemberController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $memberRepository->add($member, true);
 
-            return $this->redirectToRoute('app_member_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('admin_member_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('member/new.html.twig', [
+        return $this->renderForm('admin/admin_member/new.html.twig', [
             'member' => $member,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_member_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Member $member): Response
     {
-        return $this->render('member/show.html.twig', [
+        return $this->render('admin/admin_member/show.html.twig', [
             'member' => $member,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_member_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/modifier', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Member $member, MemberRepository $memberRepository): Response
     {
         $form = $this->createForm(MemberType::class, $member);
@@ -57,22 +57,22 @@ class MemberController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $memberRepository->add($member, true);
 
-            return $this->redirectToRoute('app_member_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('admin_member_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('member/edit.html.twig', [
+        return $this->renderForm('admin/admin_member/edit.html.twig', [
             'member' => $member,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_member_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Member $member, MemberRepository $memberRepository): Response
     {
         if ($this->isCsrfTokenValid('delete' . $member->getId(), $request->request->get('_token'))) {
             $memberRepository->remove($member, true);
         }
 
-        return $this->redirectToRoute('app_member_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('admin_member_index', [], Response::HTTP_SEE_OTHER);
     }
 }
