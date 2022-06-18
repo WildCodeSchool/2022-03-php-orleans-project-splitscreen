d6fdc3dc8982c1d55bb1cce20ba2ee98a8233fda
daniel <daniel@daniel-Inspiron-7786>
2022-06-18 10:31:50 +0200

    création de l'entity Member et création du CRUD d'ajout

---
+++ src/Entity/Member.php
@@ -0,0 +1,66 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\MemberRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: MemberRepository::class)]
+#[ORM\Table(name: '`member`')]
+class Member
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column(type: 'integer')]
+    private int $id;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $lastName;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $fisrtName;
+
+    #[ORM\Column(type: 'string', length: 255)]
+    private string $image;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getLastName(): ?string
+    {
+        return $this->lastName;
+    }
+
+    public function setLastName(string $lastName): self
+    {
+        $this->lastName = $lastName;
+
+        return $this;
+    }
+
+    public function getFisrtName(): ?string
+    {
+        return $this->fisrtName;
+    }
+
+    public function setFisrtName(string $fisrtName): self
+    {
+        $this->fisrtName = $fisrtName;
+
+        return $this;
+    }
+
+    public function getImage(): ?string
+    {
+        return $this->image;
+    }
+
+    public function setImage(string $image): self
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+}
---
+++ src/Form/MemberType.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Member;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+
+class MemberType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('lastName', TextType::class, [
+                'label' => 'Lastname',
+            ])
+            ->add('fisrtName', TextType::class, [
+                'label' => 'Firstname',
+            ])
+            ->add('image', TextType::class, [
+                'label' => 'Image',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Member::class,
+        ]);
+    }
+}
