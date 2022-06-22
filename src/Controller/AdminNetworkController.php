1c0f8b129429a700ad38df04df056dee726e3f1f
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-06-22 16:31:03 +0200

    moved and fixed all routes

--- src/Controller/AdminNetworkController.php
+++ src/Controller/AdminNetworkController.php
@@ -10,18 +10,18 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/network')]
+#[Route('/admin/network', name: 'admin_network_')]
 class AdminNetworkController extends AbstractController
 {
-    #[Route('/', name: 'app_admin_network_index', methods: ['GET'])]
+    #[Route('/', name: 'index', methods: ['GET'])]
     public function index(NetworkRepository $networkRepository): Response
     {
-        return $this->render('admin_network/index.html.twig', [
+        return $this->render('admin/admin_network/index.html.twig', [
             'networks' => $networkRepository->findAll(),
         ]);
     }
 
-    #[Route('/new', name: 'app_admin_network_new', methods: ['GET', 'POST'])]
+    #[Route('/new', name: 'new', methods: ['GET', 'POST'])]
     public function new(Request $request, NetworkRepository $networkRepository): Response
     {
         $network = new Network();
@@ -31,24 +31,24 @@ class AdminNetworkController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $networkRepository->add($network, true);
 
-            return $this->redirectToRoute('app_admin_network_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('admin_network_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('admin_network/new.html.twig', [
+        return $this->renderForm('admin/admin_network/new.html.twig', [
             'network' => $network,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_admin_network_show', methods: ['GET'])]
+    #[Route('/{id}', name: 'show', methods: ['GET'])]
     public function show(Network $network): Response
     {
-        return $this->render('admin_network/show.html.twig', [
+        return $this->render('admin/admin_network/show.html.twig', [
             'network' => $network,
         ]);
     }
 
-    #[Route('/{id}/edit', name: 'app_admin_network_edit', methods: ['GET', 'POST'])]
+    #[Route('/{id}/edit', name: 'edit', methods: ['GET', 'POST'])]
     public function edit(Request $request, Network $network, NetworkRepository $networkRepository): Response
     {
         $form = $this->createForm(NetworkType::class, $network);
@@ -57,22 +57,22 @@ class AdminNetworkController extends AbstractController
         if ($form->isSubmitted() && $form->isValid()) {
             $networkRepository->add($network, true);
 
-            return $this->redirectToRoute('app_admin_network_index', [], Response::HTTP_SEE_OTHER);
+            return $this->redirectToRoute('admin/admin_network_index', [], Response::HTTP_SEE_OTHER);
         }
 
-        return $this->renderForm('admin_network/edit.html.twig', [
+        return $this->renderForm('admin/admin_network/edit.html.twig', [
             'network' => $network,
             'form' => $form,
         ]);
     }
 
-    #[Route('/{id}', name: 'app_admin_network_delete', methods: ['POST'])]
+    #[Route('/{id}', name: 'delete', methods: ['POST'])]
     public function delete(Request $request, Network $network, NetworkRepository $networkRepository): Response
     {
         if ($this->isCsrfTokenValid('delete' . $network->getId(), $request->request->get('_token'))) {
             $networkRepository->remove($network, true);
         }
 
-        return $this->redirectToRoute('app_admin_network_index', [], Response::HTTP_SEE_OTHER);
+        return $this->redirectToRoute('admin/admin_network_index', [], Response::HTTP_SEE_OTHER);
     }
 }
