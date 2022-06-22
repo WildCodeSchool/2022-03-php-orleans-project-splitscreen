c6b48f2a3e15ab1aae8970cad6af282695bb3655
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-06-22 17:37:11 +0200

    resolved issues

--- src/Controller/AdminNetworkController.php
+++ src/Controller/AdminNetworkController.php
@@ -10,7 +10,7 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
-#[Route('/admin/network', name: 'admin_network_')]
+#[Route('/admin/reseaux', name: 'admin_network_')]
 class AdminNetworkController extends AbstractController
 {
     #[Route('/', name: 'index', methods: ['GET'])]
