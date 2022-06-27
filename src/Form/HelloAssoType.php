5122a58b383a155836026ac9f7170961c3b981d0
Mathias Bretonneau <Mathias.bretonneau@gmail.com>
2022-06-27 16:49:25 +0200

    Fix my Englishhhhh

--- src/Form/HelloAssoType.php
+++ src/Form/HelloAssoType.php
@@ -4,6 +4,7 @@ namespace App\Form;
 
 use App\Entity\HelloAsso;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
@@ -12,7 +13,9 @@ class HelloAssoType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('link')
+            ->add('link', TextType::class, [
+                'label' => 'Lien',
+            ])
         ;
     }
 
