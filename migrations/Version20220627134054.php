b42abe97a8699addee66bf184cf3377196eee147
Mathias Bretonneau <Mathias.bretonneau@gmail.com>
2022-06-27 15:42:42 +0200

    Fix entity

--- migrations/Version20220627134054.php
+++ migrations/Version20220627134054.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220627093654 extends AbstractMigration
+final class Version20220627134054 extends AbstractMigration
 {
     public function getDescription(): string
     {
--- src/Entity/HelloAsso.php
+++ src/Entity/HelloAsso.php
@@ -4,6 +4,7 @@ namespace App\Entity;
 
 use App\Repository\HelloAssoRepository;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: HelloAssoRepository::class)]
 class HelloAsso
@@ -13,6 +14,7 @@ class HelloAsso
     #[ORM\Column(type: 'integer')]
     private int $id;
 
+    #[Assert\NotBlank]
     #[ORM\Column(type: 'string', length: 255)]
     private string $link;
 
