cc17c27e679dd4ac28c92c8c3beaa335a8877575
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-06-28 12:21:39 +0200

    added fixtures for networks

--- src/DataFixtures/NetworkFixtures.php
+++ src/DataFixtures/NetworkFixtures.php
@@ -5,15 +5,35 @@ namespace App\DataFixtures;
 use App\Entity\Network;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\Validator\Constraints\Length;
 
 class NetworkFixtures extends Fixture
 {
+    private const NETWORKS = [
+        'Facebook',
+        'Instagram',
+        'Twitter',
+        'Discord',
+        'Linkedin',
+        'Tiktok',
+    ];
+    private const LINKS = [
+        'https://www.facebook.com/splitscreenasso/',
+        ' https://www.instagram.com/splitscreenasso',
+        'https://twitter.com/SplitScreenn?t=7T9yIfazpk4ATZ2uhpn6FA&s=09',
+        ' https://discord.gg/ftJeetZT9j',
+        'https://www.linkedin.com/company/splitscreenasso',
+        '',
+    ];
     public function load(ObjectManager $manager): void
     {
-        $network = new Network();
-        $network->setTitle('Facebook');
-        $network->setLink('https://www.facebook.com/splitscreenasso/');
-        $manager->persist($network);
+        for ($i = 0; $i < count(self::NETWORKS); $i++) {
+            $network = new Network();
+            $network->setTitle(self::NETWORKS[$i]);
+            $network->setLink(self::LINKS[$i]);
+            $manager->persist($network);
+            $this->addReference('networks' . $i, $network);
+        }
         $manager->flush();
     }
 }
