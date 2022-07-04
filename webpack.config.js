90278cc66b26ced1bc5e8f6622587418d5055984
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-07-04 11:40:58 +0200

    readded route for webpack for css

--- webpack.config.js
+++ webpack.config.js
@@ -45,6 +45,7 @@ Encore
     .addStyleEntry('home', './assets/styles/home.scss')
     .addStyleEntry('navbar', './assets/styles/navbar.scss')
     .addStyleEntry('banner', './assets/styles/banner.scss')
+    .addStyleEntry('homeAdmin', './assets/styles/homeAdmin.scss')
 
     // enables the Symfony UX Stimulus bridge (used in assets/bootstrap.js)
     .enableStimulusBridge('./assets/controllers.json')
