2ed044c5f9acef437744eeeddae2031ce3c0fee4
codelande <corentin-delande_student2022@wilder.school>
2022-07-04 17:04:14 +0200

    bugfix

--- migrations/Version20220704150253.php
+++ migrations/Version20220704150253.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20220704085405 extends AbstractMigration
+final class Version20220704150253 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,12 +20,14 @@ final class Version20220704085405 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE actuality CHANGE image_name image VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE actuality ADD updated_at DATETIME DEFAULT NULL, CHANGE image image VARCHAR(255) DEFAULT NULL');
+        $this->addSql('ALTER TABLE `member` DROP updated_at');
     }
 
     public function down(Schema $schema): void
     {
         // this down() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE actuality CHANGE image image_name VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE actuality DROP updated_at, CHANGE image image VARCHAR(255) NOT NULL');
+        $this->addSql('ALTER TABLE `member` ADD updated_at DATETIME NOT NULL');
     }
 }
