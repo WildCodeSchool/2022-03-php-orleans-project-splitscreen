fbcef363263bf0cf72bc368b410c93dc458a707c
Dangiras Hausmanas <dan.hausmans@gmail.com>
2022-07-04 11:55:02 +0200

    removed id

--- src/DataFixtures/NetworkFixtures.php
+++ src/DataFixtures/NetworkFixtures.php
@@ -11,32 +11,26 @@ class NetworkFixtures extends Fixture
 {
     public const NETWORKS = [
         [
-            'id' => 0,
             'title' => 'Facebook',
             'link' => 'https://www.facebook.com/splitscreenasso'
         ],
         [
-            'id' => 1,
             'title' => 'Instagram',
             'link' => 'https://www.instagram.com/splitscreenasso'
         ],
         [
-            'id' => 2,
             'title' => 'Twitter',
             'link' => 'https://twitter.com/SplitScreenn?t=7T9yIfazpk4ATZ2uhpn6FA&s=09'
         ],
         [
-            'id' => 3,
             'title' => 'Discord',
             'link' => 'https://discord.gg/ftJeetZT9j'
         ],
         [
-            'id' => 4,
             'title' => 'Linkedin',
             'link' => 'https://www.linkedin.com/company/splitscreenasso'
         ],
         [
-            'id' => 5,
             'title' => 'TikTok',
             'link' => ''
         ],
@@ -49,7 +43,7 @@ class NetworkFixtures extends Fixture
             $network->setTitle($networkItems['title']);
             $network->setLink($networkItems['link']);
             $manager->persist($network);
-            $this->addReference('network_' . $networkItems['id'], $network);
+            $this->addReference('network_' . $networkItems['title'], $network);
         }
 
         $manager->flush();
