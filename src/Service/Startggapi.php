419b2217bd77c0e8511e3b04469678b7c788bd16
codelande <corentin-delande_student2022@wilder.school>
2022-07-06 16:47:20 +0200

    m

--- src/Service/Startggapi.php
+++ src/Service/Startggapi.php
@@ -6,25 +6,50 @@ use Symfony\Contracts\HttpClient\HttpClientInterface;
 
 class Startggapi
 {
-    private HttpClientInterface $client;
+//     private HttpClientInterface $client;
 
-    public function __construct(HttpClientInterface $client)
-    {
-        $this->client = $client;
-    }
+//     public function __construct(HttpClientInterface $client)
+//     {
+//         $this->client = $client;
+//     }
 
-    public function fetchResultTournament(): array
-    {
-        $response = $this->client->request(
-            'GET',
-            'https://api.github.com/repos/symfony/symfony-docs'
-        );
+//     public function fetchResultTournament(): array
+//     {
 
-        // $statusCode = $response->getStatusCode();
-        // $contentType = $response->getHeaders()['content-type'][0];
-        $content = $response->getContent();
-        $content = $response->toArray();
+//         $query = <<<'GRAPHQL'
+//         query TournamentId($tourneySlug: String!) {
+//             tournament(slug: $tourneySlug) {
+//               id
+//             }
+//           },
+//         GRAPHQL;
 
-        return $content;
-    }
+//         $rep = $this->graphql_query('https://api.start.gg/gql/alpha', $query, ['tourneySlug' => 'test-4510'],
+//  '7b0c90d42b1fcdf13346421af9e949f3');
+
+//         dd($rep);
+//         exit;
+//     }
+
+//     public function graphql_query(string $endpoint, string $query,
+//  array $variables = [], ?string $token = null): array
+//     {
+//         $headers = ['Content-Type: application/json', 'User-Agent: Dunglas\'s minimal GraphQL client'];
+//         if (null !== $token) {
+//             $headers[] = "Authorization: bearer $token";
+//         }
+
+//         if (false === $data = @file_get_contents($endpoint, false, stream_context_create([
+//             'http' => [
+//                 'method' => 'POST',
+//                 'header' => $headers,
+//                 'content' => json_encode(['query' => $query, 'variables' => $variables]),
+//             ]
+//         ]))) {
+//             $error = error_get_last();
+//             throw new \ErrorException($error['message'], $error['type']);
+//         }
+
+//         return json_decode($data, true);
+//     }
 }
