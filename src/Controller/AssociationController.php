a50aadeda1719ecb5787b19b9415fa6bc42b7f6a
codelande <corentin-delande_student2022@wilder.school>
2022-07-06 14:32:35 +0200

    m

--- src/Controller/AssociationController.php
+++ src/Controller/AssociationController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\MemberRepository;
 use App\Repository\PartnerRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,13 +11,16 @@ use Symfony\Component\Routing\Annotation\Route;
 class AssociationController extends AbstractController
 {
     #[Route('/association', name: 'association_index')]
-    public function index(PartnerRepository $partnerRepository): Response
+    public function index(PartnerRepository $partnerRepository, MemberRepository $memberRepository): Response
     {
         $partners = $partnerRepository->findby([
         ]);
 
+        $members = $memberRepository->findAll();
+
         return $this->render('association/index.html.twig', [
             'partners' => $partners,
+            'members' => $members,
         ]);
     }
 }
