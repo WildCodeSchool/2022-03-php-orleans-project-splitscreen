09f486db67cf15635016a8fc5d87ea8ba3673434
daniel <daniel@daniel-Inspiron-7786>
2022-07-07 16:41:50 +0200

    modification de la table picture pour gérer l'upload d'event

--- src/Entity/Picture.php
+++ src/Entity/Picture.php
@@ -2,10 +2,15 @@
 
 namespace App\Entity;
 
-use App\Repository\PictureRepository;
+use DateTimeImmutable;
 use Doctrine\ORM\Mapping as ORM;
+use App\Repository\PictureRepository;
+use Symfony\Component\HttpFoundation\File\File;
+use Vich\UploaderBundle\Mapping\Annotation as Vich;
+use Symfony\Component\Validator\Constraints as Assert;
 
 #[ORM\Entity(repositoryClass: PictureRepository::class)]
+#[Vich\Uploadable]
 class Picture
 {
     #[ORM\Id]
@@ -13,25 +18,55 @@ class Picture
     #[ORM\Column(type: 'integer')]
     private int $id;
 
-    #[ORM\Column(type: 'string', length: 255)]
-    private string $name;
+    #[Vich\UploadableField(mapping: 'pictures_image', fileNameProperty: 'image')]
+    #[Assert\File(
+        maxSize: '1024k',
+        mimeTypes: ['image/jpeg', 'image/png', 'image/webp'],
+    )]
+    private ?File $imageFile = null;
+
+    #[ORM\Column(type: 'string', length: 255, nullable: true)]
+    private ?string $imageName = null;
+
+
+    #[ORM\Column(type: 'datetime', nullable: true)]
+    private ?\DateTimeInterface $updatedAt = null;
 
     #[ORM\ManyToOne(targetEntity: Event::class, inversedBy: 'pictures')]
     private Event $event;
+
+    public function __construct()
+    {
+        $this->updatedAt = new DateTimeImmutable();
+    }
+
+    public function setImageFile(?File $imageFile = null): void
+    {
+        $this->imageFile = $imageFile;
+
+        if (null !== $imageFile) {
+            $this->updatedAt = new DateTimeImmutable();
+        }
+    }
+
+    public function getImageFile(): ?File
+    {
+        return $this->imageFile;
+    }
 
     public function getId(): ?int
     {
         return $this->id;
     }
 
-    public function getName(): ?string
+    public function getimageName(): ?string
     {
-        return $this->name;
+        return $this->imageName;
     }
 
-    public function setName(string $name): self
+    public function setimageName(string $imageName): self
     {
-        $this->name = $name;
+        $this->imageName = $imageName;
 
         return $this;
     }
