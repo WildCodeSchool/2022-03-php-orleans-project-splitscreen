2b8cbb60a75930c8e99d82d0538a803cf2ec9978
codelande <corentin-delande_student2022@wilder.school>
2022-07-07 17:28:05 +0200

    m

--- src/Controller/ApiTestController.php
+++ src/Controller/ApiTestController.php
@@ -9,15 +9,4 @@ use Symfony\Component\Routing\Annotation\Route;
 
 class ApiTestController extends AbstractController
 {
-    #[Route('/api/test', name: 'app_api_test')]
-    public function index(Startggapi $startggapi, string $slug): Response
-    {
-        $rankings = $startggapi->fetchResultTournament(
-            '$slug'
-        );
-
-        return $this->render('api_test/index.html.twig', [
-            'rankings' => $rankings,
-        ]);
-    }
 }
--- src/Service/Startggapi.php
+++ src/Service/Startggapi.php
@@ -6,56 +6,57 @@ use GuzzleHttp\Client;
 
 class Startggapi
 {
-    public function fetchResultTournament(string $slug): array
-    {
-
-        $query = <<<GQL
-        query EventQuery(\$slug: String, \$page: Int!, \$perPage: Int!) {
-            event(slug: \$slug){
-        id
-        name
-        videogame {
-          name
-        }
-        standings(query: {
-          perPage: \$perPage,
-          page: \$page
-                }){
-                  nodes {
-                    placement
-                    entrant {
-                      id
-                      name
-                    }
-                  }
-                }
-              }
-            },
-      GQL;
-
-        $graphqlEndpoint = 'https://api.start.gg/gql/alpha';
-        $client = new Client();
-        $accessToken = '7b0c90d42b1fcdf13346421af9e949f3';
-        $response = $client->request('POST', $graphqlEndpoint, [
-            'headers' => [
-                'Content-Type' => 'application/json',
-                'Authorization' => 'Bearer ' . $accessToken,
-            ],
-            'json' => [
-                'query' => $query,
-                'variables' => [
-                    'slug' => $slug,
-                    'page' => 1,
-                    "perPage" => 10
-                ]
-            ]
-        ]);
-        $json = $response->getBody()->getContents();
-
-        $body = json_decode($json);
-        $data = $body->data;
-        $rankings = $data->event->standings->nodes;
-
-        return $rankings;
-    }
+  // public function fetchResultTournament(string $slug): array
+  // {
+    // $query = <<<GQL
+    //         query EventQuery(\$slug: String, \$page: Int!, \$perPage: Int!) {
+    //             event(slug: \$slug){
+    //         id
+    //         name
+    //         videogame {
+    //           name
+    //         }
+    //         standings(query: {
+    //           perPage: \$perPage,
+    //           page: \$page
+    //                 }){
+    //                   nodes {
+    //                     placement
+    //                     entrant {
+    //                       id
+    //                       name
+    //                     }
+    //                   }
+    //                 }
+    //               }
+    //             },
+    //       GQL;
+    // $graphqlEndpoint = 'https://api.start.gg/gql/alpha';
+    // $client = new Client();
+    // $accessToken = '7b0c90d42b1fcdf13346421af9e949f3';
+    // $response = $client->request('POST', $graphqlEndpoint, [
+    //   'headers' => [
+    //     'Content-Type' => 'application/json',
+    //     'Authorization' => 'Bearer ' . $accessToken,
+    //   ],
+    //   'json' => [
+    //     'query' => $query,
+    //     'variables' => [
+    //       'slug' => $slug,
+    //       'page' => 1,
+    //       "perPage" => 10
+    //     ]
+    //   ]
+    // ]);
+  //   $json = $response->getBody()->getContents();
+  //   $body = json_decode($json);
+  //   $data = $body->data;
+  //   $rankings = $data->event->standings->nodes;
+  //   return $rankings;
+  // }
+  // public function extractSlug(string $fullSlug): string
+  // {
+  //   $slug = explode('/', $fullSlug);
+  //   return $slug[0];
+  // }
 }
