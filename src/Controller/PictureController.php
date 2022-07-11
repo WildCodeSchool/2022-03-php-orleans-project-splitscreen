370b1b0261b48b7f6cd0044391a9ea437e8fa15d
daniel <daniel@daniel-Inspiron-7786>
2022-07-11 15:34:53 +0200

    S04HNC49_uploads_image_galeire

---
+++ src/Controller/PictureController.php
@@ -0,0 +1,79 @@
+<?php
+
+namespace App\Controller;
+
+use App\Entity\Event;
+use App\Entity\Picture;
+use App\Form\PictureType;
+use App\Repository\PictureRepository;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Annotation\Route;
+
+#[Route('/picture')]
+class PictureController extends AbstractController
+{
+    #[Route('/{id}', name: 'app_picture_index', methods: ['GET'])]
+    public function index(PictureRepository $pictureRepository, Event $event): Response
+    {
+        return $this->render('picture/index.html.twig', [
+            'pictures' => $pictureRepository->findBy(['event' => $event]),
+        ]);
+    }
+
+    #[Route('/new', name: 'app_picture_new', methods: ['GET', 'POST'])]
+    public function new(Request $request, PictureRepository $pictureRepository): Response
+    {
+        $picture = new Picture();
+        $form = $this->createForm(PictureType::class, $picture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $pictureRepository->add($picture, true);
+
+            return $this->redirectToRoute('app_picture_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('picture/new.html.twig', [
+            'picture' => $picture,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_picture_show', methods: ['GET'])]
+    public function show(Picture $picture): Response
+    {
+        return $this->render('picture/show.html.twig', [
+            'picture' => $picture,
+        ]);
+    }
+
+    #[Route('/{id}/edit', name: 'app_picture_edit', methods: ['GET', 'POST'])]
+    public function edit(Request $request, Picture $picture, PictureRepository $pictureRepository): Response
+    {
+        $form = $this->createForm(PictureType::class, $picture);
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $pictureRepository->add($picture, true);
+
+            return $this->redirectToRoute('app_picture_index', [], Response::HTTP_SEE_OTHER);
+        }
+
+        return $this->renderForm('picture/edit.html.twig', [
+            'picture' => $picture,
+            'form' => $form,
+        ]);
+    }
+
+    #[Route('/{id}', name: 'app_picture_delete', methods: ['POST'])]
+    public function delete(Request $request, Picture $picture, PictureRepository $pictureRepository): Response
+    {
+        if ($this->isCsrfTokenValid('delete' . $picture->getId(), $request->request->get('_token'))) {
+            $pictureRepository->remove($picture, true);
+        }
+
+        return $this->redirectToRoute('app_picture_index', [], Response::HTTP_SEE_OTHER);
+    }
+}
---
+++ src/Form/PictureType.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\Picture;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Vich\UploaderBundle\Form\Type\VichFileType;
+
+class PictureType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('imageFile', VichFileType::class, [
+                'label' => 'Image',
+            ])
+        ;
+    }
+
+    public function configureOptions(OptionsResolver $resolver): void
+    {
+        $resolver->setDefaults([
+            'data_class' => Picture::class,
+        ]);
+    }
+}
