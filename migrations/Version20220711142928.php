2fdef61ab8388a1c20a61a8014cc4c205cd68a4c
codelande <corentin-delande_student2022@wilder.school>
2022-07-11 16:37:32 +0200

    m

---
+++ migrations/Version20220711142928.php
@@ -0,0 +1,31 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20220711142928 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE picture ADD updated_at DATETIME NOT NULL, CHANGE image_name image_name VARCHAR(255) DEFAULT NULL');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE picture DROP updated_at, CHANGE image_name image_name VARCHAR(255) NOT NULL');
+    }
+}
